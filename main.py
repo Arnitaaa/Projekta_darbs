16919d34b140392f04d26345ed58a3c1805c6241
Arnitaaa <148310632+Arnitaaa@users.noreply.github.com>
2025-02-23 21:01:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 pip install Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/preili")
+def preili():
+    return render_template("preili.html")
+
+@app.route("/pasaule")
+def pasaule():
+    return render_template("pasaule.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
